21a3265b13dae6ee4f4d7e3138ad973405c1d3b5
eltonnvls <167933155+eltonnvls@users.noreply.github.com>
2025-09-20 10:15:51 -0300

    Update exercicio.php

--- exercicio.php
+++ exercicio.php
@@ -63,3 +63,35 @@ class BoletoBancoItau extends BoletoAbstrato implements BoletoComJurosInterface 
              . $this->dataVencimento->format('d/m/Y');
     }
 }
+
+/*Parte 1: Conceitos (Valor: 3,0 pontos)
+a) Qual é a finalidade da BoletoInterface neste sistema? Por que é importante usá-la?
+    
+ A BoletoInterface define um contrato que todas as classes de boleto devem seguir.
+Ela garante que qualquer banco (Brasil, Caixa, Itaú etc.) terá os mesmos métodos obrigatórios (gerarCodigoBarras, validar, renderizar).
+Isso é importante porque:
+Permite padronização das regras.
+Facilita polimorfismo (podemos tratar qualquer boleto como “BoletoInterface”).
+Ajuda na manutenção e extensibilidade: se amanhã criar um BoletoCaixa, ele já sabe quais métodos precisa implementar.
+
+b) Explique por que a classe BoletoAbstrato precisa ser declarada como abstract. O que aconteceria se ela não fosse abstrata?
+
+BoletoAbstrato serve como modelo base para todos os boletos, trazendo:
+Propriedades e métodos comuns (valor, vencimento, renderizar).
+Métodos abstratos que cada banco deve implementar (renderizarHtml, renderizarPdf).
+Se ela não fosse abstract:
+Seria possível instanciá-la diretamente, o que não faz sentido, pois um “boleto genérico” não existe.
+Além disso, os métodos que dependem de implementação específica (renderizarHtml, renderizarPdf) ficariam sem definição, quebrando a execução.
+
+c) O método renderizar () é implementado na classe abstrata, mas delega a execução para métodos abstratos. Qual é a vantagem dessa abordagem em termos de reuso e manutenção?    
+
+ A vantagem é aplicar o template method:
+A lógica comum de renderização (se for HTML chama X, se for PDF chama Y) fica centralizada na classe abstrata.
+Cada banco só precisa se preocupar com sua forma específica de renderizar (HTML/PDF).
+Isso promove reuso de código (não precisa repetir a mesma estrutura em todas as classes) e facilidade de manutenção (se a regra de escolha mudar, só ajusta na abstrata).*/  
+    
+    
+    
+    
+    
+
